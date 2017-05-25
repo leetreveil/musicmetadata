3e92b23cebc8806b6a27e0db81c892d8c161e2ff
Peter Rietveld <Tarabass@users.noreply.github.com>
2017-05-25 15:04:03 +0200

    Fix line length
    Hopefully now it's okay then

--- test/test-not-pipable.js
+++ test/test-not-pipable.js
@@ -4,7 +4,9 @@ var test = require('tape')
 test('non-pipable', function (t) {
   t.plan(3)
 
-  mm('https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg', function (err, result) {
+  var url = 'https://upload.wikimedia.org/wikipedia/en/4/45/ACDC_-_Back_In_Black-sample.ogg'
+
+  mm(url, function (err, result) {
     t.notEqual(err, undefined)
     t.equal(err.toString(), 'TypeError: stream.pipe is not a function')
     t.equal(result, undefined)
